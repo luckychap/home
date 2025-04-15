e6bac61c2fd329fc6973ab80b5c27049b370780a
Lakatos Martin <lakatos.martin@scheidt-bachmann.sk>
2025-04-15 13:16:30 +0200

    I tried to add krewfile and tweak its configuration, but it's not working yet.

---
+++ apps/krewfile.nix
@@ -0,0 +1,21 @@
+# https://github.com/brumhard/krewfile
+
+  inputs.krewfile = {
+    url = "github:brumhard/krewfile";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+
+{ inputs, pkgs, ... }: {
+  imports = [
+    inputs.krewfile.homeManagerModules.krewfile
+  ];
+
+  programs.krewfile = {
+    enable = true;
+    krewPackage = pkgs.krew;
+    plugins = [
+      "get-all"
+    ];
+  };
+}
