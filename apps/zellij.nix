9c7daeaa08876ea487ffd6615e6e90cd1861fd02
Martin Lakatoš <lakatos.martin@gmail.com>
2025-09-01 08:34:37 +0200

    ZELLIJ: Change scroll mode keybinding from Ctrl+s to Ctrl+f. When using K9s, this keybinding made issue to save logs.

--- apps/zellij.nix
+++ apps/zellij.nix
@@ -4,6 +4,23 @@
     settings = {
       copy_command = "xclip -selection clipboard";
       show_startup_tips = false;
+      keybinds = {
+        shared_except = {
+          _args = [
+            "scroll"
+            "locked"
+          ];
+          bind = {
+            _args = ["Ctrl f"];
+            _children = [
+              { SwitchToMode = "Scroll";}
+            ];
+          };
+          unbind = {
+            _args = ["Ctrl s"];
+          };
+        };
+      };
     };
   };
 }
