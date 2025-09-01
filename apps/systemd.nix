cd3905522df80fb0650c29b774531513d004a47f
Martin Lakatoš <lakatos.martin@gmail.com>
2025-09-01 23:35:34 +0200

    SYSTEMD: playing around with user mount

--- apps/systemd.nix
+++ apps/systemd.nix
@@ -2,15 +2,18 @@
     systemd.user = {
         enable = true;
         mounts = {
-            home-lakatos-nas = {
+            "home-${builtins.getEnv "USER"}-mnt-nas_01" = {
                 Unit = {
                     Description = "Mount S&B network storage - NAS 01";
+                    After = "network-online.target";
+                    Wants = "network-online.target";
                 };
                 Mount = {
                     What = "//fza-nas-01.sb-sk.local/FCS-BTS";
-                    Where = "/home/lakatos/nas/";
+                    Where = "/home/${builtins.getEnv "USER"}/mnt/nas_01";
                     Type = "cifs";
-                    Options = "credentials=/etc/cifs-utils/.cifs-credentials-lakatos,rw,uid=1000,gid=1000";
+                    Options = "credentials=/home/${builtins.getEnv "USER"}/.config/cifs-utils/.cifs-credentials-lakatos,rw,uid=1000,gid=1000";
+#                    Options = "credentials=/home/${builtins.getEnv "USER"}/.config/cifs-utils/.cifs-credentials-lakatos,rw";
                     TimeoutSec = 30;
                 };
             };
