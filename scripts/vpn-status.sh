26a4ec1335b9986944a8f7e6ab3da9af299b4db1
Martin Lakatoš <lakatos.martin@gmail.com>
2026-05-14 22:43:34 +0200

    GNOME-ARGOS: Change script for the VPN status.

--- scripts/vpn-status.sh
+++ scripts/vpn-status.sh
@@ -1,12 +1,7 @@
-#/usr/bin/env bash
-# set -x
-
-# echo "VPN"
-# echo "---"
+#!/usr/bin/env bash
 
 status=$(globalprotect show --status | awk '{print $4}')
 
-# echo ${#status}
 if [ "$status" = "Connected" ]; then
   echo "🌞"
 else
